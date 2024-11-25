a229d39c60a364cdac554c165df57d54f017b1ed
Burnett Miao <searchfor1987@gmail.com>
2024-11-25 14:23:54 +0800

    header finish

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('menu-btn');
+  const navLinks = document.getElementById('nav-links');
+  const menuBtnIcon = menuBtn.querySelector('i');
+
+  menuBtn.addEventListener('click', (e) => {
+    navLinks.classList.toggle('open');
+
+    const isOpen = navLinks.classList.contains('open');
+    menuBtnIcon.setAttribute(
+      'class',
+      isOpen ? 'ri-close-line' : 'ri-menu-line'
+    );
+  });
+
+  navLinks.addEventListener('click', (e) => {
+    navLinks.classList.remove('open');
+    menuBtnIcon.setAttribute('class', 'ri-menu-line');
+  });
+
+  // Scroll reveal
+  const scrollRevealOption = {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+  };
+
+  ScrollReveal().reveal('.header__image img', {
+    ...scrollRevealOption,
+    origin: 'right',
+  });
+
+  ScrollReveal().reveal('.header__content h1', {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal('.header__content p', {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal('.header__btns', {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+});
